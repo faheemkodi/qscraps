b83cde21118a7052c73c5a05f5c0b1f1e4f5a3f2
Faheem Kodi <faheemkodi@gmail.com>
2021-06-15 15:57:38 +0530

    Listing component final

--- frontend/src/components/Listing.js
+++ frontend/src/components/Listing.js
@@ -5,40 +5,30 @@ import { Card, Image, Row, Col } from 'react-bootstrap';
 const Listing = ({ listing }) => {
   return (
     <Card className="my-2 my-sm-3 listing">
-      <Card.Header className="text-center font-weight-bold lead small text-uppercase text-primary">
+      <Card.Header className="font-weight-bold small text-uppercase text-primary">
         {listing.title}
       </Card.Header>
       <Card.Body>
         <LinkContainer to={`/listing/${listing._id}`}>
           <Row>
-            <Col xs={3} className="d-flex flex-col align-items-center">
+            <Col xs={4} className="my-auto">
               <Image
+                fluid
                 thumbnail
                 src={listing.coverImage}
                 alt="listing cover image"
               />
             </Col>
-            <Col
-              xs={9}
-              className="text-center text-uppercase d-flex flex-col align-items-center"
-            >
-              <Col xs={6} className="text-muted">
-                <p className="small text-center font-weight-bold">
-                  {listing.make}
-                </p>
-                <p className="small text-center font-weight-bold">
-                  {listing.model}
-                </p>
-                <p className="small text-center font-weight-bold">
-                  {listing.year.join(', ')}
-                </p>
-              </Col>
+            <Col xs={8} className="text-uppercase my-auto">
+              <p className="font-weight-bold my-auto">{listing.make}</p>
+              <p className="my-auto">{listing.model}</p>
+              <p className="small font-weight-bold my-auto">
+                {listing.year.join(', ')}
+              </p>
 
-              <Col xs={6}>
-                <p className="small text-center text-secondary font-weight-bold">
-                  {listing.category.join(', ')}
-                </p>
-              </Col>
+              <p className="small text-secondary my-auto">
+                {listing.category.join(', ')}
+              </p>
             </Col>
           </Row>
         </LinkContainer>
