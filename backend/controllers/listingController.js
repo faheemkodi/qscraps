5adef12eb553c6deef7f8ad2b30e8882a71749bc
Faheem Kodi <faheemkodi@gmail.com>
2021-06-17 18:39:20 +0530

    lg ui

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -5,7 +5,7 @@ import Listing from '../models/listingModel.js';
 // @route GET/api/listings
 // @access Public route
 const getListings = asyncHandler(async (req, res) => {
-  const pageSize = 15;
+  const pageSize = 16;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
